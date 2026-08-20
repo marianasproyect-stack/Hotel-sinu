3bdeb250d66596811d0a33ebb3618d32dd0bce17
Mariana Viggiani <marianasproyect@gmail.com>
2026-08-20 11:09:14 -0500

    Configura API publica de Render en frontend

--- static/static/js/app.js
+++ static/static/js/app.js
@@ -1,5 +1,5 @@
 ﻿
-const API = "http://localhost:8081/api";
+const API = "https://hotel-sinu.onrender.com/api";
 
 let usuarioActual = (() => {
   const guardado = localStorage.getItem("usuarioActual");
